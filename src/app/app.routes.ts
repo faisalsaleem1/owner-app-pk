92ad52ffbd00bc94d04c47ff566d48e0db0237c4
faisalsaleem1 <faisal.saleem@universalplus.technology>
2024-12-22 23:19:31 +0400

    latest code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -21,7 +21,7 @@ export const routes: Routes = [
       {
         path: "dashboard",
         component: DashboardComponent,
-        // canActivate: [AuthGuard],
+        canActivate: [AuthGuard],
       },
       {
         path: "user-detail",
