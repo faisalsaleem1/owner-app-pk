84d5bf8aeba117a0d26163ddf83775aedb2f879f
faisalsaleem1 <faisal.saleem@universalplus.technology>
2024-12-24 16:40:28 +0400

    latest code

--- src/app/theme/components/bet-reports/bet-reports.component.ts
+++ src/app/theme/components/bet-reports/bet-reports.component.ts
@@ -1,87 +1,60 @@
-import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
-import { TimeFilterTabsComponent } from '../../../common/time-filter-tabs/time-filter-tabs.component';
-import { ApiService } from '../../../services/api.service';
-import { ConditionhandlerService } from '../../../services/conditionhandler.service';
+import { NgFor } from "@angular/common";
+import { Component } from "@angular/core";
+import { TimeFilterTabsComponent } from "../../../common/time-filter-tabs/time-filter-tabs.component";
+import { ApiService } from "../../../services/api.service";
+import { ConditionhandlerService } from "../../../services/conditionhandler.service";
 
 @Component({
-  selector: 'app-bet-reports',
+  selector: "app-bet-reports",
   imports: [NgFor, TimeFilterTabsComponent],
-  templateUrl: './bet-reports.component.html',
-  styleUrl: './bet-reports.component.scss'
+  templateUrl: "./bet-reports.component.html",
+  styleUrl: "./bet-reports.component.scss",
 })
 export class BetReportsComponent {
   betReports: any = [];
   startDate: any;
   endDate: any;
   currentPath: any;
+  tableContentData: any[] | any;
+
+  tableHeaderData: string[] = [
+    "Sports Name",
+    "Company Name",
+    "Settle",
+    "UnSettle",
+    "Void",
+    "Rejected",
+  ];
 
   constructor(
-      private apiService: ApiService,
-      private conditionhandler: ConditionhandlerService
-    ) {}
-  
-    ngOnInit(): void {
-      this.conditionhandler.getRangeData().subscribe((resp: any) => {
-        const { currentRoute, date } = resp;
-        this.currentPath = currentRoute;
-        const { from = null, to = null } = date || {};
-        const forStartDate = new Date(from);
-        this.startDate = forStartDate.toISOString();
-        const forEndDate = new Date(to);
-        this.endDate = forEndDate.toISOString();
-      });
-      this.getBetReport();
-    }
-  
-    getBetReport() {
-      let payload = {
-        startDate: this.startDate,
-        endDate: this.endDate,
-      };
-      this.apiService.getBetReports(payload).subscribe((apiResp) => {
-        this.tableContentData = apiResp.data;
-      });
-    }
-  tableHeaderData: string[] = [
-    'Sports Name',
-    'Company Name',
-    'Settle',
-    'UnSettle',
-    'Viod',
-    'Rejected',
-  ];
+    private apiService: ApiService,
+    private conditionhandler: ConditionhandlerService
+  ) {}
 
-  // Table content data
-  tableContentData: any[] = [
-    {
-      sportsName: 'Cricket',
-      companyName: 'T10Exchnage',
-      settle: '10',
-      unSettle: '10',
-      viod: '10',
-      rejected:'10',
-    },
-    {
-      sportsName: 'Cricket',
-      companyName: 'T10Exchnage',
-      settle: '10',
-      unSettle: '10',
-      viod: '10',
-      rejected:'10',
-    },
-    {
-      sportsName: 'Cricket',
-      companyName: 'T10Exchnage',
-      settle: '10',
-      unSettle: '10',
-      viod: '10',
-      rejected:'10',
-    },
-  ];
+  ngOnInit(): void {
+    this.conditionhandler.getRangeData().subscribe((resp: any) => {
+      const { currentRoute, date } = resp;
+      this.currentPath = currentRoute;
+      const { from = null, to = null } = date || {};
+      const forStartDate = new Date(from);
+      this.startDate = forStartDate.toISOString();
+      const forEndDate = new Date(to);
+      this.endDate = forEndDate.toISOString();
+    });
+    this.getBetReport();
+  }
+
+  getBetReport() {
+    let payload = {
+      startDate: this.startDate,
+      endDate: this.endDate,
+    };
+    this.apiService.getBetReports(payload).subscribe((apiResp) => {
+      this.tableContentData = apiResp.data;
+    });
+  }
 
   trackByIndex(index: number): number {
     return index;
   }
-
 }
--- src/app/theme/components/pl-reports/pl-reports.component.ts
+++ src/app/theme/components/pl-reports/pl-reports.component.ts
@@ -1,80 +1,57 @@
-import { Component } from '@angular/core';
-import { TimeFilterTabsComponent } from '../../../common/time-filter-tabs/time-filter-tabs.component';
-import { NgFor } from '@angular/common';
-import { ApiService } from '../../../services/api.service';
-import { ConditionhandlerService } from '../../../services/conditionhandler.service';
+import { Component } from "@angular/core";
+import { TimeFilterTabsComponent } from "../../../common/time-filter-tabs/time-filter-tabs.component";
+import { NgFor } from "@angular/common";
+import { ApiService } from "../../../services/api.service";
+import { ConditionhandlerService } from "../../../services/conditionhandler.service";
 
 @Component({
-  selector: 'app-pl-reports',
+  selector: "app-pl-reports",
   imports: [NgFor, TimeFilterTabsComponent],
-  templateUrl: './pl-reports.component.html',
-  styleUrl: './pl-reports.component.scss'
+  templateUrl: "./pl-reports.component.html",
+  styleUrl: "./pl-reports.component.scss",
 })
 export class PlReportsComponent {
   plReport: any = [];
   startDate: any;
   endDate: any;
   currentPath: any;
+  tableContentData: any[] | any;
+
+  tableHeaderData: string[] = [
+    "Sports Name",
+    "Company Name",
+    "20/12/2024",
+    "20/12/2024",
+    "20/12/2024",
+  ];
 
   constructor(
-      private apiService: ApiService,
-      private conditionhandler: ConditionhandlerService
-    ) {}
-  
-    ngOnInit(): void {
-      this.conditionhandler.getRangeData().subscribe((resp: any) => {
-        const { currentRoute, date } = resp;
-        this.currentPath = currentRoute;
-        const { from = null, to = null } = date || {};
-        const forStartDate = new Date(from);
-        this.startDate = forStartDate.toISOString();
-        const forEndDate = new Date(to);
-        this.endDate = forEndDate.toISOString();
-      });
-      this.getPlReport();
-    }
-  
-    getPlReport() {
-      let payload = {
-        startDate: this.startDate,
-        endDate: this.endDate,
-      };
-      this.apiService.getPlReports(payload).subscribe((apiResp) => {
-        this.tableContentData = apiResp.data;
-      });
-    }
-  tableHeaderData: string[] = [
-    'Sports Name',
-    'Company Name',
-    '20/12/2024',
-    '20/12/2024',
-    '20/12/2024',
-  ];
+    private apiService: ApiService,
+    private conditionhandler: ConditionhandlerService
+  ) {}
 
-  // Table content data
-  tableContentData: any[] = [
-    {
-      sportsName: 'Cricket',
-      companyName: 'T10Exchnage',
-      date1: '10',
-      date2: '10',
-      date3: '10',
-    },
-    {
-      sportsName: 'Cricket',
-      companyName: 'T10Exchnage',
-      date1: '10',
-      date2: '10',
-      date3: '10',
-    }, 
-    {
-      sportsName: 'Cricket',
-      companyName: 'T10Exchnage',
-      date1: '10',
-      date2: '10',
-      date3: '10',
-    },
-  ];
+  ngOnInit(): void {
+    this.conditionhandler.getRangeData().subscribe((resp: any) => {
+      const { currentRoute, date } = resp;
+      this.currentPath = currentRoute;
+      const { from = null, to = null } = date || {};
+      const forStartDate = new Date(from);
+      this.startDate = forStartDate.toISOString();
+      const forEndDate = new Date(to);
+      this.endDate = forEndDate.toISOString();
+    });
+    this.getPlReport();
+  }
+
+  getPlReport() {
+    let payload = {
+      startDate: this.startDate,
+      endDate: this.endDate,
+    };
+    this.apiService.getPlReports(payload).subscribe((apiResp) => {
+      this.tableContentData = apiResp.data;
+    });
+  }
 
   trackByIndex(index: number): number {
     return index;
